643b5ff07a5874cbf9d6c09ec5feae5f9eb3cba7
Farhan T Ansari <farhantansari@gmail.com>
2025-08-09 23:02:58 -0700

    Update BaseLoader.py

--- dataset/data_loader/BaseLoader.py
+++ dataset/data_loader/BaseLoader.py
@@ -23,6 +23,7 @@ import pandas as pd
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from retinaface import RetinaFace   # Source code: https://github.com/serengil/retinaface
+import torch
 from models.network_swinir import SwinIR as net
 
 class BaseLoader(Dataset):
