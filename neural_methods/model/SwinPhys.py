e6eb255e841cd84b2c00c0f04b7f979ab576f412
Farhan T Ansari <farhantansari@gmail.com>
2025-10-15 08:31:22 -0700

    Create SwinPhys.py

---
+++ neural_methods/model/SwinPhys.py
@@ -0,0 +1,100 @@
+from torch.utils.data import DataLoader, Dataset
+import torch
+import torch.nn as nn
+from neural_methods.model.PhysNet import PhysNet_padding_Encoder_Decoder_MAX
+
+def diff_normalize_data(data):
+  """Calculate discrete difference in video data along the time-axis and normalize by its standard deviation."""
+  n, h, w, c = data.shape
+  diffnormalized_len = n - 1
+  diffnormalized_data = np.zeros((diffnormalized_len, h, w, c), dtype=np.float32)
+  diffnormalized_data_padding = np.zeros((1, h, w, c), dtype=np.float32)
+  for j in range(diffnormalized_len):
+    diffnormalized_data[j, :, :, :] = (data[j + 1, :, :, :] - data[j, :, :, :]) / (
+        data[j + 1, :, :, :] + data[j, :, :, :] + 1e-7)
+  diffnormalized_data = diffnormalized_data / np.std(diffnormalized_data)
+  diffnormalized_data = np.append(diffnormalized_data, diffnormalized_data_padding, axis=0)
+  diffnormalized_data[np.isnan(diffnormalized_data)] = 0
+  return diffnormalized_data
+
+class ImageDataSet(Dataset):
+  def __init__(self, frames, window_size = 7):
+    self.frames = frames
+    self.window_size = window_size
+
+  def __len__(self):
+    return len(self.frames)
+
+  def __getitem__(self, idx):
+    frame = self.frames[idx]
+    frame = frame.astype(np.float32)/ 255
+    frame = frame.transpose(2, 0, 1)  # HWC-RGB to CHW-RGB
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    frame = torch.from_numpy(frame).float().to(device)
+    # pad input image to be a multiple of window_size
+    _, h_old, w_old = frame.size()
+    h_pad = (h_old // self.window_size + 1) * self.window_size - h_old
+    w_pad = (w_old // self.window_size + 1) * self.window_size - w_old
+    frame = torch.cat([frame, torch.flip(frame, [1])], 1)[:, :h_old + h_pad, :]
+    frame = torch.cat([frame, torch.flip(frame, [2])], 2)[:, :, :w_old + w_pad]
+    return frame
+
+class SwinIR(nn.Module):
+  def __init__(self, swinir_model_path, window_size=7, img_size=126):
+    super(SwinIR, self).__init__()
+    self.swinir_model = load_swinir_model(MODEL_PATH_40, window_size=window_size, img_size=img_size)
+    self.window_size = window_size
+    self.img_size = img_size
+    # Freeze parameters of the model
+    for param in self.swinir_model.parameters():
+      param.requires_grad = False
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    self.swinir_model.to(device)
+
+  def forward(self, frames):
+    height, width, channel = frames[0].shape
+    #print(frames.shape)
+    image_ds = ImageDataSet(frames, self.window_size)
+    image_dl = DataLoader(image_ds, batch_size=50, shuffle=False)
+    restored_frames = []
+    for batch in image_dl:
+      restored = self.swinir_model(batch)
+      for i in range(restored.shape[0]):
+        output = restored[i]
+        output = output[..., :height, :width]
+        output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
+        if output.ndim == 3:
+          output = output.transpose(1, 2, 0)  # CHW-RGB to HWC-RGB
+          output = (output * 255.0).round().astype(np.uint8)  # float32 to uint8
+        restored_frames.append(output)
+      print(output.shape)
+      media.show_image(output)
+    return np.array(restored_frames)
+
+class SwinPhys(nn.Module):
+  def __init__(self, swinir_model_path, restore=True, physnet_model_path="", window_size=7, img_size=126, num_frames=50):
+    super(SwinPhys, self).__init__()
+    self.swinir_model = SwinIR(swinir_model_path, window_size=window_size, img_size=img_size)
+    self.window_size = window_size
+    self.img_size = img_size
+    self.restore = restore
+    
+    # Load pretrained physnet
+    if physnet_model_path != "":
+      self.physnet_model = load_physnet_model(physnet_model_path, num_frames=num_frames)
+      device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+      self.physnet_model.to(device)
+
+  def forward(self, frames):
+    if self.restore:
+      restored_frames = self.swinir_model(frames) # N, H, W, C
+    else:
+      restored_frames = frames
+      print(restored_frames.shape)
+    restored_frames = diff_normalize_data(restored_frames) # N, H, W, C
+    print(restored_frames.shape)
+    # Transpose to get data in the form C, N, H, W
+    restored_frames = restored_frames.transpose(3, 0, 1, 2)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    restored_frames = torch.from_numpy(restored_frames).float().unsqueeze(0).to(device) 
+    return self.physnet_model(restored_frames)
