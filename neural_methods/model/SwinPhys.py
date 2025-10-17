652e849079f409bed7545cc3501144481e1d4468
Farhan T Ansari <farhantansari@gmail.com>
2025-10-16 18:59:24 -0700

    Remove debug print statements in SwinPhys.py
    Commented out print statements for debugging output.

--- neural_methods/model/SwinPhys.py
+++ neural_methods/model/SwinPhys.py
@@ -122,7 +122,7 @@ class SwinIR(nn.Module):
                     output = output.transpose(1, 2, 0)  # CHW-RGB to HWC-RGB
                     output = (output * 255.0).round().astype(np.uint8)  # float32 to uint8
                     restored_frames.append(output)
-            print("Restored batch")
+            #print("Restored batch")
             #media.show_image(output)
         return np.array(restored_frames)
 
@@ -152,18 +152,15 @@ class SwinPhys(nn.Module):
     def forward(self, x):
         [batch, channel, length, width, height] = x.shape
 
-        print("Input shape (CNWH): " + x.shape)
         restored_frames = x
         # Assume batch size of 1
         if self.restore:
             frames = x.squeeze().float().cpu().numpy()   # C, N, W, H
             frames = frames.transpose(1, 2, 3, 0)   # N, W, H, C
             restored_frames = self.swinir_model(frames) # # N, W, H, C
-            print("Restored shape (NWHC): " + restored_frames.shape)
         restored_frames = diff_normalize_data(restored_frames) # N, W, H, C
         # Transpose to get data in the form C, N, W, H
         restored_frames = restored_frames.transpose(3, 0, 1, 2)
-        print("After diff normalization (CNWH): " + restored_frames.shape)
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         restored_frames = torch.from_numpy(restored_frames).float().unsqueeze(0).to(device) # batch_size, C, N, W, H
         return self.physnet_model(restored_frames)
