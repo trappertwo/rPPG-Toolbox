3d4eb0d9b4270346e424f3e418b8f5027314f1be
Farhan T Ansari <farhantansari@gmail.com>
2025-10-18 16:19:34 -0700

    Refactor PhysnetTrainer class with updated imports

--- neural_methods/trainer/PhysnetTrainer.py
+++ neural_methods/trainer/PhysnetTrainer.py
@@ -157,7 +157,8 @@ class PhysnetTrainer(BaseTrainer):
         if self.config.TOOLBOX_MODE == "only_test":
             if not os.path.exists(self.config.INFERENCE.MODEL_PATH):
                 raise ValueError("Inference model path error! Please check INFERENCE.MODEL_PATH in your yaml.")
-            self.model.load_state_dict(torch.load(self.config.INFERENCE.MODEL_PATH))
+            if not self.config.MODEL.PHYSNET.DO_RESTORE:
+                self.model.load_state_dict(torch.load(self.config.INFERENCE.MODEL_PATH))
             print("Testing uses pretrained model!")
             print(self.config.INFERENCE.MODEL_PATH)
         else:
@@ -209,6 +210,7 @@ class PhysnetTrainer(BaseTrainer):
             self.model_dir, self.model_file_name + '_Epoch' + str(index) + '.pth')
         torch.save(self.model.state_dict(), model_path)
         print('Saved Model Path: ', model_path)
+
 
 
 
