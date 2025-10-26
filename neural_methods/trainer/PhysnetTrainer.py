772f497561848fff63ea0b91ea944b5064ed3241
Farhan T Ansari <farhantansari@gmail.com>
2025-10-26 06:26:15 -0700

    Update PhysnetTrainer.py

--- neural_methods/trainer/PhysnetTrainer.py
+++ neural_methods/trainer/PhysnetTrainer.py
@@ -35,7 +35,8 @@ class PhysnetTrainer(BaseTrainer):
             # SwinIR pretrained model has image size 126
             self.model = SwinPhys(swinir_model_path, restore=config.MODEL.PHYSNET.DO_RESTORE, physnet_model_path=config.INFERENCE.MODEL_PATH,
                                   diff_normalize=config.MODEL.PHYSNET.SWINPHYS_DIFF_NORMALIZE, window_size=7, img_size=126, num_frames=config.MODEL.PHYSNET.FRAME_NUM,
-                                  freeze_swinir=config.MODEL.PHYSNET.FREEZE_SWINIR, freeze_physnet=config.MODEL.PHYSNET.FREEZE_PHYSNET) # [3, T, 128,128]
+                                  freeze_swinir=config.MODEL.PHYSNET.FREEZE_SWINIR, freeze_physnet=config.MODEL.PHYSNET.FREEZE_PHYSNET,
+                                  model_type=config.MODEL.PHYSNET.SWIN_MODEL_TYPE) # [3, T, 128,128]
         else:
             self.model = PhysNet_padding_Encoder_Decoder_MAX(
                 frames=config.MODEL.PHYSNET.FRAME_NUM).to(self.device)  # [3, T, 128,128]
@@ -210,6 +211,7 @@ class PhysnetTrainer(BaseTrainer):
             self.model_dir, self.model_file_name + '_Epoch' + str(index) + '.pth')
         torch.save(self.model.state_dict(), model_path)
         print('Saved Model Path: ', model_path)
+
 
 
 
